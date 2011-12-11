662ee4e94b0d476299d2af17147f1718a46b48d1
C. Flores <flores@petalphile.com>
2011-12-11 15:05:41 -0800

    move js out of the layout

---
+++ public/js/ajax.js
@@ -0,0 +1,12 @@
+var options={ 
+	target: '#result',
+	success: function(){
+		return false; 
+	},
+	error: function(xhr){
+		$('#result').html(xhr.responseText);
+	},
+};
+
+$('#yanc').ajaxForm(options);
+
---
+++ public/js/toggle_input_fields.js
@@ -0,0 +1,43 @@
+onload=function () {
+	var source = document.getElementsByName('source');
+	for (i=0; i < source.length; i++) {
+		pie = source[i];
+		if (pie.checked) {
+			showDiv(pie.value);
+		};
+	};
+
+	var method = document.getElementsByName('method');
+	for (i=0; i < method.length; i++) {
+		pie = method[i];
+		if (pie.checked) {
+			showDiv(pie.value);
+		};
+	};
+
+	var calc_for = document.getElementById('calc_for1');
+	if (calc_for.selectedIndex) {
+		pie = calc_for.selectedIndex;
+		showDiv(calc_for.options[pie].value);
+	};
+
+	var premix_calc_for = document.getElementById('premix_calc_for');
+	if (premix_calc_for.selectedIndex) {
+		pie = premix_calc_for.selectedIndex;
+		showDiv(premix_calc_for.options[pie].value);
+	};
+};
+
+function showDiv(method) {
+	document.getElementById('dump').style.display = "none"; 
+	document.getElementById('premix_dump').style.display = "none";
+	document.getElementById('target').style.display = "none";
+	document.getElementById('premix_target').style.display = "none";
+	if (method && document.getElementById(method)) {
+		document.getElementById(method).style.display = "block";
+	}
+	if (method && document.getElementsByName(method)) {
+		document.getElementsByName(method).checked = true;
+	}
+};
+
